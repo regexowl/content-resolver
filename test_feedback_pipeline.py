bf5601e56feee81cdf90342675348d9690b38546
Klara Simickova <kaya.simickova@gmail.com>
2021-11-18 14:07:45 +0100

    changed folder path

--- test_feedback_pipeline.py
+++ test_feedback_pipeline.py
@@ -4,7 +4,7 @@ import feedback_pipeline
 
 def create_mock_settings():
     settings = {}
-    settings["configs"] = "code/input"
+    settings["configs"] = "code/input/configs"
     settings["allowed_arches"] = ["armv7hl","aarch64","ppc64le","s390x","x86_64"]
 
 def test_build_completion():
