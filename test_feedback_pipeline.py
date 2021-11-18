bdbc2575aab25a3d6abcd84ad96bd7a952244726
Klara Simickova <kaya.simickova@gmail.com>
2021-11-18 14:14:38 +0100

    fixed test

--- test_feedback_pipeline.py
+++ test_feedback_pipeline.py
@@ -7,8 +7,10 @@ def create_mock_settings():
     settings["configs"] = "code/input/configs"
     settings["allowed_arches"] = ["armv7hl","aarch64","ppc64le","s390x","x86_64"]
 
+    return settings
+
 def test_build_completion():
     assert 1 == 1
 
 def test_get_configs():
-    feedback_pipeline.get_configs(feedback_pipeline.load_settings(create_mock_settings()))
+    feedback_pipeline.get_configs(create_mock_settings())
