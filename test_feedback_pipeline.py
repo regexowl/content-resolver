007b60d87c996006a3fe69cc5d9a3260711134a4
Klara Simickova <kaya.simickova@gmail.com>
2021-11-18 14:01:49 +0100

    added mock settings

--- test_feedback_pipeline.py
+++ test_feedback_pipeline.py
@@ -2,8 +2,13 @@
 
 import feedback_pipeline
 
+def create_mock_settings():
+    settings = {}
+    settings["configs"] = "code/input"
+    settings["allowed_arches"] = ["armv7hl","aarch64","ppc64le","s390x","x86_64"]
+
 def test_build_completion():
     assert 1 == 1
 
 def test_get_configs():
-    feedback_pipeline.get_configs(feedback_pipeline.load_settings())
+    feedback_pipeline.get_configs(feedback_pipeline.load_settings(create_mock_settings()))
