8901b3ee8b1d9580e2f0a51fbd847b921d9c4363
regexowl <49452678+regexowl@users.noreply.github.com>
2021-11-30 16:18:47 +0100

    Update test_config_files.py

--- test_config_files.py
+++ test_config_files.py
@@ -4,7 +4,7 @@ import feedback_pipeline
 
 def create_mock_settings():
     settings = {}
-    settings["configs"] = "code/input/configs"
+    settings["configs"] = "input/configs"
     settings["allowed_arches"] = ["armv7hl","aarch64","ppc64le","s390x","x86_64"]
 
     return settings
