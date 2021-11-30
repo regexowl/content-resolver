741f124ec7b9c01b79d6bde48932530079f9c522
regexowl <49452678+regexowl@users.noreply.github.com>
2021-11-30 18:47:12 +0100

    Update test_feedback_pipeline.py

--- test_feedback_pipeline.py
+++ test_feedback_pipeline.py
@@ -2,15 +2,5 @@
 
 import feedback_pipeline
 
-def create_mock_settings():
-    settings = {}
-    settings["configs"] = "code/input/configs"
-    settings["allowed_arches"] = ["armv7hl","aarch64","ppc64le","s390x","x86_64"]
-
-    return settings
-
 def test_build_completion():
     assert 1 == 1
-
-def test_get_configs():
-    feedback_pipeline.get_configs(create_mock_settings())
